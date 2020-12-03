aaeb435aa4e05ff5d3a533c3532a6a806aa4b2a7
dnov09 <dngige9@gmail.com>
2020-12-02 22:57:25 -0600

    more map changes - plus started map js code

--- js/map.js
+++ js/map.js
@@ -3,9 +3,10 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 7.8549495909436216,
+            lng: -1.3847759802873594
         },
+
         zoom: 8,
     });
 }

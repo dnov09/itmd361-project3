911d0a7b4fe38d64c22876ff07e4021c33ab9f65
dnov09 <dngige9@gmail.com>
2020-12-02 23:39:07 -0600

    placed markers

--- js/map.js
+++ js/map.js
@@ -1,12 +1,43 @@
-let map;
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 7.8549495909436216,
-            lng: -1.3847759802873594
-        },
+    var ghanaLoc = new google.maps.LatLng(7.8549495909436216, -1.3847759802873594);
+    var options = {
+        center: ghanaLoc,
+        zoom: 7,
+        zoomControl: true,
+        gestureHandling: "greedy"
+    }
+    var map = new google.maps.Map(document.getElementById("map"), options);
 
-        zoom: 8,
+    // locations
+    var accraLoc = new google.maps.LatLng(5.603033427043894, -0.18559112601790112);
+    var akropongLoc = new google.maps.LatLng(5.972838467401452, -0.08741863396091402);
+    var parkLoc = new google.maps.LatLng(9.397312180630133, -1.7473248046233434);
+    var mountLoc = new google.maps.LatLng(7.146181130651107, 0.35227486137596364);
+    
+    // placing markers
+    var accraMarker = new google.maps.Marker({
+        position: accraLoc,
+        map,
+        title: "Accra, The Capital",
     });
+
+    var akrpngMarker = new google.maps.Marker({
+        position: akropongLoc,
+        map,
+        title: "Presby Church at my Mother's hometown.",
+    });
+
+    var parkMarker = new google.maps.Marker({
+        position: parkLoc,
+        map,
+        title: "Mole National Park",
+    });
+
+    var mountMarker = new google.maps.Marker({
+        position: mountLoc,
+        map,
+        title: "Mountain Afadjato",
+    });
+
+    // on-click functions
 }

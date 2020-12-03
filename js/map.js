aa50938cdce3957b9ce4d43dc6cc3124e85db4bd
dnov09 <dngige9@gmail.com>
2020-12-03 00:06:09 -0600

    marker changes - changed icons - added animation

--- js/map.js
+++ js/map.js
@@ -13,30 +13,39 @@ function initMap() {
     var akropongLoc = new google.maps.LatLng(5.972838467401452, -0.08741863396091402);
     var parkLoc = new google.maps.LatLng(9.397312180630133, -1.7473248046233434);
     var mountLoc = new google.maps.LatLng(7.146181130651107, 0.35227486137596364);
-    
+
     // placing markers
     var accraMarker = new google.maps.Marker({
         position: accraLoc,
         map,
         title: "Accra, The Capital",
+        icon: 'https://maps.google.com/mapfiles/kml/pal4/icon47.png',
+        animation: google.maps.Animation.BOUNCE
+
     });
 
     var akrpngMarker = new google.maps.Marker({
         position: akropongLoc,
         map,
         title: "Presby Church at my Mother's hometown.",
+        icon: 'https://maps.google.com/mapfiles/kml/pal2/icon11.png',
+        // animation: google.maps.Animation.DROP
     });
 
     var parkMarker = new google.maps.Marker({
         position: parkLoc,
         map,
         title: "Mole National Park",
+        icon: 'https://maps.google.com/mapfiles/kml/pal2/icon4.png',
+        // animation: google.maps.Animation.BOUNCE
     });
 
     var mountMarker = new google.maps.Marker({
         position: mountLoc,
         map,
         title: "Mountain Afadjato",
+        icon: 'https://maps.google.com/mapfiles/kml/pal3/icon29.png',
+        // animation: google.maps.Animation.BOUNCE
     });
 
     // on-click functions
